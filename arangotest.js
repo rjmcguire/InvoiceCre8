c98c3de8e17dd673df31769f7d9d1b994b0b3435
Michael Bruyninckx <michael@modul8.com>
2014-01-27 19:16:04 +0100

    modified testscript with input of Anders Elo.

--- arangotest.js
+++ arangotest.js
@@ -1,9 +1,7 @@
 var arango = require("arango");
+var db = arango.Connection("http://raspberrypi/InvoiceCre8_MODUL8com:example");
 
-var db = arango.Connection({_name:"InvoiceCre8_MODUL8com", _collection : "example", _server : { hostname : "raspberrypi" } });
-
-
-
+/*
 db.document.list().then(function(res) {
     console.log(res);
     for(var index in res.documents) {
@@ -16,4 +14,27 @@ db.document.list().then(function(res) {
     }
 }, function(err) {
     console.log("ERROR: %j", err);
+});
+*/
+
+db.document.list().then(function (res) {
+    return res.documents.map(function (doc) {
+        return doc.replace("/_api/document/","")
+    });
+}, function (err) {
+    console.log("LIST ERROR: %j", err);
+}).then(function (documents) {
+    db.batch.start();
+    for(var index in documents) {
+        db.document.get(documents[index]);
+    }
+    return db.batch.exec();
+}, function (err) {
+    console.log("BATCH ERROR: %j", err);
+}).then(function (res) {
+    for(var index = 1; index <= res[0].length; index++) {
+        console.log(JSON.stringify(res[index], null, 2));
+    }
+}, function (error) {
+    console.log("RESULTS ERROR: %j", err);
 });
