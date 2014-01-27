74f6b17afdd9e6d13d6086277d0f789a5ea3c25a
Michael Bruyninckx <michael@modul8.com>
2014-01-27 19:19:26 +0100

    removed commented old parts

--- arangotest.js
+++ arangotest.js
@@ -1,22 +1,6 @@
 var arango = require("arango");
 var db = arango.Connection("http://raspberrypi/InvoiceCre8_MODUL8com:example");
 
-/*
-db.document.list().then(function(res) {
-    console.log(res);
-    for(var index in res.documents) {
-        console.log("fetching: %j", res.documents[index].replace("/_api/document/",""));
-        db.document.get(res.documents[index].replace("/_api/document/","")).then(function(res) {
-            console.log(JSON.stringify(res, null, 2));
-        }, function(err) {
-            console.log("ERROR: %j", err);
-        });
-    }
-}, function(err) {
-    console.log("ERROR: %j", err);
-});
-*/
-
 db.document.list().then(function (res) {
     return res.documents.map(function (doc) {
         return doc.replace("/_api/document/","")
